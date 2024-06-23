f279bcc4dea373f266a2e0664583b518d68b5129
Guilherme Attilio <guilhermeciello@gmail.com>
2024-06-23 20:42:42 -0300

    inclusao da datatable

--- meusAutores.php
+++ meusAutores.php
@@ -20,13 +20,18 @@ while ($row = $result->fetch_assoc()) {
 
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Biblioteca Virtual</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="css/styles.css">
+    <script src="css/jquery/jquery-3.6.0.min.js"></script>
+    <link rel="stylesheet" href="css/datatables/datatables.min.css">
+    <script src="css/datatables/datatables.min.js"></script>
 </head>
+
 <body>
     <nav class="navbar navbar-expand-lg">
         <div class="container-fluid">
@@ -48,12 +53,14 @@ while ($row = $result->fetch_assoc()) {
                         </a>
                         <ul class="dropdown-menu">
                             <li><a class="dropdown-item" href="cadastrarLivro.php">Livros</a></li>
-                            <li><hr class="dropdown-divider"></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
                             <li><a class="dropdown-item" href="cadastrarAutor.php">Autores</a></li>
                         </ul>
                     </li>
                     <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="index.php">Meus Autores</a>
+                        <a class="nav-link" aria-current="page" href="index.php">Meus Autores</a>
                     </li>
                 </ul>
             </div>
@@ -69,7 +76,7 @@ while ($row = $result->fetch_assoc()) {
                 </div>
                 <hr>
                 <?php if (!empty($autores)) { ?>
-                    <table class="table table-striped table-bordered">
+                    <table class="table table-striped table-bordered" id="table_autores">
                         <thead class="table-dark">
                             <tr>
                                 <th>Nome</th>
@@ -80,19 +87,19 @@ while ($row = $result->fetch_assoc()) {
                             </tr>
                         </thead>
                         <tbody>
-                        <?php foreach ($autores as $autor) { ?>
-                            <tr>
-                                <td><?=$autor['nome']?></td>
-                                <td class="text-center"><?=$autor['dataNascimento']?></td>
-                                <td class="text-center"><?=$autor['nacionalidade']?></td>
-                                <td><?=$autor['bibliografia']?></td>
-                                <td class="text-end">
-                                    <a href="editarAutor.php?id=<?= $autor['id'] ?>"><i class="fa-regular fa-pen-to-square me-2"></i>Editar</a>
-                                    <span class="mx-2 opacity-50">|</span>
-                                    <a href="deletarAutor.php?id=<?= $autor['id'] ?>"><i class="fa-solid fa-trash-can me-2"></i>Eliminar</a>
-                                </td>
-                            </tr>
-                        <?php } ?>
+                            <?php foreach ($autores as $autor) { ?>
+                                <tr>
+                                    <td><?= $autor['nome'] ?></td>
+                                    <td class="text-center"><?= $autor['dataNascimento'] ?></td>
+                                    <td class="text-center"><?= $autor['nacionalidade'] ?></td>
+                                    <td><?= $autor['bibliografia'] ?></td>
+                                    <td class="text-end">
+                                        <a href="editarAutor.php?id=<?= $autor['id'] ?>"><i class="fa-regular fa-pen-to-square me-2"></i>Editar</a>
+                                        <span class="mx-2 opacity-50">|</span>
+                                        <a href="deletarAutor.php?id=<?= $autor['id'] ?>"><i class="fa-solid fa-trash-can me-2"></i>Eliminar</a>
+                                    </td>
+                                </tr>
+                            <?php } ?>
                         </tbody>
                     </table>
                     <div class="row">
@@ -108,4 +115,38 @@ while ($row = $result->fetch_assoc()) {
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
+
 </html>
+<script>
+    $(document).ready(function() {
+        // datatable
+        $('#table_autores').DataTable({
+            pageLength: 10,
+            pagingType: "full_numbers",
+            language: {
+                decimal: "",
+                emptyTable: "Sem dados disponíveis na tabela.",
+                info: "Mostrando _START_ até _END_ de _TOTAL registros",
+                infoEmpty: "Mostrando 0 até 0 de 0 registros",
+                infoFiltered: "(Filtrando MAX total de registros)",
+                infoPostFix: "",
+                thousands: ",",
+                lengthMenu: "Mostrando _MENU_ registros por página.",
+                loadingRecords: "Carregando...",
+                processing: "Processando...",
+                search: "Filtrar:",
+                zeroRecords: "Nenhum registro encontrado.",
+                paginate: {
+                    first: "Primeira",
+                    last: "Última",
+                    next: "Seguinte",
+                    previous: "Anterior"
+                },
+                aria: {
+                    sortAscending: ": ative para classificar a coluna em ordem crescente.",
+                    sortDescending: ": ative para classificar a coluna em ordem decrescente."
+                }
+            }
+        });
+    });
+</script>
